a9aba91cf3a78b8db2f51cc2f6db3b8257816d27
Sarah Caplan <caplan.sarah@gmail.com>
2017-11-01 15:59:09 -0700

    Fix typo in association

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -2,7 +2,7 @@ class Resource < ApplicationRecord
   has_many :resource_tags
   has_many :tags, through: :resource_tags
   has_many :favorites
-  has_many :fans, though: :favorites
+  has_many :fans, through: :favorites
   belongs_to :author, class_name: "User"
 
   validates :title, :url, :author, presence: true
