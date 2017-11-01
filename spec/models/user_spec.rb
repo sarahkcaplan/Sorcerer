c2c8a349c092d517b104dabca610e263d1f4010b
Iris Nevins <ikn02010@gmail.com>
2017-11-01 16:47:26 -0700

    Complete writing tests for user associations. One test failing, unresolved

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -54,6 +54,51 @@ describe User do
       User.create(first_name: "Sarah", last_name: "Caplan", email: "sarah@gmail.com", password: "1234")
       expect(User.all).to be_empty
     end
+  end
 
+  describe "association between users and resources that they create" do
+    let(:user) {User.create(first_name: "Sarah", last_name: "Caplan", email: "sarah@gmail.com", password: "1234", user_type: "Student")}
+
+    let!(:resource) {user.resources_posted.create(title: "Google", url: "google.com")}
+
+    it "allows a user to create a resource" do
+      expect(user.resources_posted.first).to eq(resource)
+    end
+
+    it "allows a user to create a resource with the author_id matching the user's id" do
+      expect(resource.author_id).to eq(user.id)
+    end
+
+    it "allows a user to create a resource with the author_id matching the user's id" do
+      expect(resource.author_id).to eq(user.id)
+    end
+  end
+
+  describe "association between users and resources that they favorite" do
+    let(:user) {User.create(first_name: "Sarah", last_name: "Caplan", email: "sarah@gmail.com", password: "1234", user_type: "Student")}
+
+    let!(:resource1) {Resource.create(title: "Google", url: "google.com", author_id: 1)}
+
+    let!(:resource2) {Resource.create(title: "Amazon", url: "amazon.com", author_id: 1)}
+
+    let!(:favorite1) {user.favorites.create(fan_id: user.id, resource_id: resource1.id) }
+
+    let!(:favorite2) {user.favorites.create(fan_id: user.id, resource_id: resource2.id) }
+
+    it "allows a user to favorite a resource" do
+      expect(user.favorites.first).to eq(favorite1)
+    end
+
+    it "allows a user to have a list of favorites" do
+      expect(user.favorites).to include(favorite1, favorite2)
+    end
+
+    it "allows a user to have a list of favorited resources " do
+      expect(user.resources_favorited).to include(resource1, resource2)
+    end
   end
 end
+
+
+
+
