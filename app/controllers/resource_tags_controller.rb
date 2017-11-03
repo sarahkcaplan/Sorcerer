8952b57c74aeb1cce8d3bd01a97fda703b535284
Sarah Caplan <caplan.sarah@gmail.com>
2017-11-03 12:06:52 -0700

    Add a tag and add tag validation

--- app/controllers/resource_tags_controller.rb
+++ app/controllers/resource_tags_controller.rb
@@ -7,13 +7,18 @@ class ResourceTagsController < ApplicationController
   end
 
   def create
-    tag = Tag.find_or_create_by(name: tag_params)
-    @resource_tag = ResourceTag.new(resource: params[:resource_id], tag: tag.id)
-    if @resource_tag.save
+    find_tag
+    @resource_tag = ResourceTag.new(resource: find_resource, tag: find_tag)
+    if @resource_tag.tag_id == nil
+      @errors = ["This tag is already associated with the resource"]
+      @tag = Tag.new(name: find_tag.name)
+      render :new
+    elsif @resource_tag.save
       redirect_to user_path(current_user)
     else
-      @errors = @resource_tag.errors.full_messages
-      render :create
+      @errors = ["This tag is already associated with the resource"]
+      @tag = Tag.new(name: find_tag.name)
+      render :new
     end
   end
 
@@ -27,7 +32,18 @@ class ResourceTagsController < ApplicationController
     #ResourceTag.destroy(resource_tag_id)
   end
 
-  def tag_params
-    params.permit(:name)
+  private
+
+  def tag_attributes
+    params.require(:tag).permit(:name, :permission)
   end
+
+  def find_tag
+    @tag = Tag.find_or_create_by(tag_attributes)
+  end
+
+  def find_resource
+    @resource = Resource.find(params[:resource_id])
+  end
+
 end
