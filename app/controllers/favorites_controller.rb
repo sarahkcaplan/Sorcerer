1db16f4c4cf2490eb50c9fa0023031b4e0da619a
Iris Nevins <ikn02010@gmail.com>
2017-11-02 19:45:38 -0700

    Create favorites vew page and add route to routes.rb

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -27,9 +27,10 @@ class FavoritesController < ApplicationController
   end
 
   def destroy
-    @favorite = Favorite.where(resource_id: params[:id], fan_id: current_user.id)
-    favorite_id = @favorite.id
-    Favorite.destroy(favorite_id)
+    @favorite = Favorite.find(params[:id])
+    p "******************"
+    p params[:id]
+    @favorite.destroy
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   #Favorites routes
   resources :resources do
     resources :favorites, only: [:create, :destroy]
   end
+  resources :favorites, only: [:index]
 
   root to: "resources#index"
 
